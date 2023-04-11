57c305f6d416876f85ba38b86e562e9ac5d65e64
Shukrullah Shirzad <shukrullah.shirzad.779@gmail.com>
2023-04-11 09:26:37 +0430

    fixed the bug in the create grid button

--- index.js
+++ index.js
@@ -3,23 +3,27 @@ let screenWidth = document.querySelector(".screen").offsetWidth;
 let screenHeight = document.querySelector(".screen").offsetHeight;
 let size = [...document.querySelectorAll(".grid-btn")];
 let selectedSize = 100;
-
+let selectedClass;
 // add event listener to size buttons
 for(item of size){
     item.addEventListener("click",(e)=>{
         selectedSize = parseInt(e.target.classList[0]);
+        selectedClass = e.target.classList[1];
     })
 }
-let squareNumber = (screenHeight * screenWidth)/selectedSize;
+
+
 function createGrid(dimension){
+    screen.textContent = "";
+    let squareNumber = (screenHeight * screenWidth)/selectedSize;
     for(let i = 0; i<squareNumber; i++){
-      let newEl = document.createElement("div");
-      screen.appendChild(newEl);
-      newEl.classList.add("grid-div");
-    }
+        let newEl = document.createElement("div");
+        screen.appendChild(newEl);
+        newEl.classList.add(selectedClass);
+      }
 }
 
 let createGridBtn = document.querySelector(".create-grid-btn");
 createGridBtn.addEventListener("click", ()=>{
-    console.log(selectedSize)
+    createGrid(selectedSize); 
 })
