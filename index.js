58b9d1966d533a8690e29f26a31b8d77651aa63d
Shukrullah Shirzad <shukrullah.shirzad.779@gmail.com>
2023-04-12 18:17:33 +0430

    add create grid, clear, and erase functions

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ let screenHeight = document.querySelector(".screen").offsetHeight;
 let size = [...document.querySelectorAll(".grid-btn")];
 let selectedSize = 100;
 let selectedClass;
+let rainbowColors = ["violet", "indigo", "blue", "green", "yellow", "orange", "red"];
 // add event listener to size buttons
 for(item of size){
     item.addEventListener("click",(e)=>{
@@ -12,10 +13,14 @@ for(item of size){
     })
 }
 
-
+function randomColorGenerator(){
+    let randomNumber = Math.floor(Math.random()*7);
+    let randomColor = rainbowColors[randomNumber];
+    return randomColor
+}
 function createGrid(dimension){
     screen.textContent = "";
-    let squareNumber = (screenHeight * screenWidth)/selectedSize;
+    let squareNumber = (screenHeight * screenWidth)/dimension;
     for(let i = 0; i<squareNumber; i++){
         let newEl = document.createElement("div");
         screen.appendChild(newEl);
@@ -27,3 +32,46 @@ let createGridBtn = document.querySelector(".create-grid-btn");
 createGridBtn.addEventListener("click", ()=>{
     createGrid(selectedSize); 
 })
+
+// change the background color of the grid
+let colorBTN = document.querySelectorAll(".btn");
+let colorChoice;
+colorBTN.forEach(item => {
+    item.addEventListener('click', (e)=>{
+        colorChoice = e.target.textContent
+    })
+});
+function changeBackground(item){
+    let colorSelected = document.querySelector(".grid-color").value;
+ 
+    if([...screen.children].includes(item.target)){
+        if(colorChoice == ""){
+            item.target.style.backgroundColor = colorSelected
+        }
+        else if(colorChoice == "rainbow"){
+            item.target.style.backgroundColor = randomColorGenerator(); 
+        }
+        else if(colorChoice == "erase"){
+            item.target.style.backgroundColor = "";
+        }
+        else{
+             item.target.style.backgroundColor = colorSelected;
+        }
+        
+    }
+   
+}
+screen.addEventListener("mousedown", (e)=>{
+    changeBackground(e)
+});
+
+// clear the screen
+
+function clearScreen(){
+    for(let i = 0; i<screen.children.length; i++){
+        screen.children[i].style.backgroundColor = "";
+    }
+}
+
+let clearBtn = document.querySelector('.clear');
+clearBtn.addEventListener('click', clearScreen);
